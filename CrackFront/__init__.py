05248f804af108967323fee2dcb9aa41a9ef532b
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-07-25 22:24:10 +0200

    MAINT: version

--- CrackFront/__init__.py
+++ CrackFront/__init__.py
@@ -23,3 +23,12 @@ peaks slightly offset wrt. to the collocation points.
 
 
 """
+
+try:
+    from importlib.metadata import version
+
+    __version__ = version(__name__)
+except ImportError:
+    from pkg_resources import get_distribution
+
+    __version__ = get_distribution(__name__).version
