abdbe453b14144bdf62c4c1fb34bb0a6b85ab3d3
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-07-03 18:31:12 +0200

    DOC: More SphinxDoc for the Crack Front on roughness model

--- CrackFront/StraightForRoughness.py
+++ CrackFront/StraightForRoughness.py
@@ -13,8 +13,9 @@ sx = 1.
 #w = # or alpha^2 / (2 E^*)
 
 class SinewaveCrackFrontLoadEnergyConstK:
-    def __init__(self, n, sy, kr_right, kr_left,w):
-        r"""
+    r"""
+        For each the left and right crack front
+
         .. math::
 
             U = \frac{1}{L} \int dz U^0(a(z)) + G_c \sum_n \frac{|q_n| L}{2}  \tilde a_n \tilde a_{-n}
@@ -23,15 +24,41 @@ class SinewaveCrackFrontLoadEnergyConstK:
 
             \frac{\delta^2 U}{\delta a}
 
-        Note that we do not implement the energy because U_0 is hard to compute in general
+        Note that we do not implement the energy because U_0 is hard to compute in general.
+
+        :math:`G_0` is the energy release rate below the straight crack:
+
+        .. math::
+
+            G_0 = \frac{1}{2E^*} \left(K_{\rm J}(a) + K_{\rm R}(a, z) \right)^2
+
+        :math:`K_{\rm J}` is the stress intensity factor in the contact of a sinewave against a flat elastic halfspace.py
+
+        Note that it depends on the mean position of the left and right crack.
+        The assymmetric solution for the sinewave contact was provided by
+        Carbone, G., Mangialardi, L., 2004. Adhesion and friction of an elastic half-space in contact with a slightly wavy rigid surface. Journal of the Mechanics and Physics of Solids 52, 1267–1287. https://doi.org/10/bjmwjw
 
 
+
+    """
+
+    def __init__(self, n, sy, kr_right, kr_left, w):
+        r"""
         Parameters
         ----------
+        n: int
+            number of collocation points on each crack front
 
+        kr_right: callable
+            When the right crack front is straight and at distance `a` from the tip of the sinusoidal indenter,
+            `kr_right(a, z)` is the stress intensity factor at `z`
 
+        kr_left: callable
+            When the left crack front is straight and at distance `a` from the tip of the sinusoidal indenter,
+            `kr_left(a, z)` is the stress intensity factor at `z`
 
-
+        w: float
+            work of adhesion, i.e. :math:`G_c=K_c^2 / 2 E^*`
         """
 
         dy = sy / n
@@ -85,13 +112,10 @@ class SinewaveCrackFrontLoadEnergyConstK:
         KJl = K_J(a_s=al, a_o=ar, P=load)
         KJr = K_J(a_s=ar, a_o=al, P=load)
 
-        K0l = KJl + self.kr_left(al, self.y)
-        K0r = KJr + self.kr_right(al, self.y)
+        dG0l_al = self.kr_left(al, self.y, der="1") + K_J(a_s=al, a_o=ar, P=load, der="1_a_s")
+        dG0l_ar = K_J(a_s=al, a_o=ar, P=load, der="1_a_o")
 
-        dG0l_al = self.kr_left(al, self.y, der="1") + K_J(a_s=al, a_o= ar, P=load, der="1_a_s")
-        dG0l_ar = K_J(a_s=al, a_o= ar, P=load, der="1_a_o")
-
-        dG0r_ar = self.kr_right(ar, self.y, der="1") + K_J(a_s=ar, a_o= al, P=load, der="1_a_s")
+        dG0r_ar = self.kr_right(ar, self.y, der="1") + K_J(a_s=ar, a_o=al, P=load, der="1_a_s")
         dG0r_al = K_J(a_s=ar, a_o=al, P=load, der="1_a_o")
 
         ldl = dG0l_al * pl + self.w * self.elastic_hessp(pl)
