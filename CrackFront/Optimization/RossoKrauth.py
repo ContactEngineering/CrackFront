06dadc14a7d88a39a6345caba2afc494deab3f84
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-10-15 16:25:55 +0200

    WIP: implementation of rosso and krauth that should be able to handle grid spacings other then 1 is equivalent to my first implementation

--- CrackFront/Optimization/RossoKrauth.py
+++ CrackFront/Optimization/RossoKrauth.py
@@ -10,11 +10,12 @@ class linear_interpolated_pinning_field:
         L, Lx = values.shape
         self.L = L
         self.Lx = Lx
+        self.period = Lx
         self.values = values
 
         self.a_below = np.zeros(L, dtype=int)
         self.a_above = np.zeros(L, dtype=int)
-
+        self.kinks = np.arange(self.Lx)
         self.indexes = np.arange(L, dtype=int)
 
     def __call__(self, a, der="0"):
@@ -37,6 +38,13 @@ class linear_interpolated_pinning_field:
         elif der == "1":
             return slope
 
+    #
+    # Alternative to ceil and floor
+    # from bisect import bisect_left
+    #
+    # https://numpy.org/doc/stable/reference/generated/numpy.searchsorted.html
+    #
+    #
 
 def brute_rosso_krauth(a, driving_a, line, gtol=1e-4, maxit=10000, dir=1):
     r"""
@@ -94,6 +102,95 @@ def brute_rosso_krauth(a, driving_a, line, gtol=1e-4, maxit=10000, dir=1):
         grad = line.gradient(a, driving_a)
 
         nit += 1
+
+
+    if nit == maxit:
+        success = False
+    else:
+        success = True
+
+    result = OptimizeResult({
+        'success': success,
+        'x': a,
+        'nit': nit,
+        })
+    return result
+
+
+def brute_rosso_krauth_other_spacing(a, driving_a, line, gtol=1e-4, maxit=10000, dir=1):
+    r"""
+    Variation of PRE 65
+
+    One requirement for this algorithm is that the starting position is purely advancing,
+    i.e. the gradient is strictly negative when we pull the line in positive `a` direction
+
+    In the original they move each pixel one after another.
+
+    Here we move them all at once, which should make better use of vectorization and parallelisation.
+
+    This might have drawbacks when the pinning field is weak, where pixels fail collectively.
+
+
+    """
+    L = line.L
+    a_test = np.zeros(L)
+    a_test[0] = 1
+    elastic_stiffness_individual = line.elastic_gradient(a_test, 0)[0]
+    # elastic reaction force when we move the pixel individually
+
+    grad = line.gradient(a, driving_a)
+    if (grad * dir > 0).any():
+        raise ValueError("Starting Configuration is not purely advancing or receding")
+
+    indexes = line.pinning_field.indexes
+    kinks = line.pinning_field.kinks
+    values = line.pinning_field.values
+    colloc_point_above = np.searchsorted(kinks, a, side="right")
+
+    period = line.pinning_field.period
+
+    grid_spacing = 1
+    # import matplotlib.pyplot as plt
+    # fig, ax = plt.subplots()
+    # l, = ax.plot(a)
+    # l_above,  = ax.plot(kinks[colloc_point_above])
+    nit = 0
+    while (np.max(abs(grad)) > gtol) and nit < maxit:
+        # print(grad)
+        # Nullify the force on each pixel, assuming it is greater then
+        pinning_field_slope = (values[indexes, colloc_point_above] - values[indexes, colloc_point_above-1]) / grid_spacing
+        grad = line.elastic_gradient(a, driving_a) \
+            + values[indexes, colloc_point_above-1] \
+            + pinning_field_slope * (a - kinks[colloc_point_above-1])
+
+        stiffness = pinning_field_slope + elastic_stiffness_individual
+
+        increment = - grad / stiffness
+        # negative stiffness generates wrong step length.
+        a_new = np.where(stiffness > 0, a + increment, a + (1 + 1e-14) * dir)
+        #colloc_point_above = np.where( )
+        a_new[grad * dir >= 0] = a[grad * dir >= 0]
+
+        if dir == 1:
+            mask_new_pixel = a_new % period >= kinks[colloc_point_above]
+            a_new[mask_new_pixel] = kinks[colloc_point_above][mask_new_pixel]
+            colloc_point_above[mask_new_pixel] += 1
+            a = a_new
+        elif dir == -1:
+            mask_new_pixel = a_new % period <= kinks[colloc_point_above-1]
+            a_new[mask_new_pixel] = kinks[colloc_point_above-1][mask_new_pixel]
+            colloc_point_above[mask_new_pixel] -= 1
+
+            a = a_new
+
+
+        #l_above.set_ydata(kinks[colloc_point_above])
+        # l.set_ydata(a)
+        # ax.set_ylim(np.min(a-1), np.max(a))
+        # plt.pause(.001)
+
+        nit += 1
+
 
 
     if nit == maxit:
@@ -286,7 +383,69 @@ def example_strong_pinning():
     ax.legend()
     plt.pause(.5)
 
+def time_RK_implementations():
+    import time
+
+    L = 2048
+    Lx = 2048
+
+    random_forces = np.random.normal(size=(L, Lx)) * 1.  # *0.05
+    pinning_field = linear_interpolated_pinning_field(random_forces)
+
+    line = ElasticLine(L, L / 16, pinning_field=pinning_field)
+
+    a_init = np.zeros(L) + 1e-14
+    a = a_init.copy()
+    w = 1
+
+    grad = line.gradient(a, w)
+
+    while (grad > 0).any():
+        w += 1
+        grad = line.gradient(a, w)
+
+    gtol = 1e-6
+
+    a_forcings = w + np.linspace(0, 1, 200)
+
+    start_time = time.time()
+
+    # %%
+
+    for rosso_krauth in [brute_rosso_krauth, brute_rosso_krauth_other_spacing]:
+        mean_a_RK = []
+        a = np.zeros(L)
+        start_time = time.time()
+        for a_forcing in a_forcings:
+            # print(a_forcing)
+            wrapped_gradient = lambda a: line.gradient(a, a_forcing)
+
+            a = rosso_krauth(a, w, line, maxit=100000, gtol=gtol, )
+            mean_a_RK.append(np.mean(a))
+        print("TIME ROSSO Krauth", time.time() - start_time)
+
 
 if __name__ == "__main__":
-    example_strong_pinning()
-    example_large_disp_force()
+    L = 4
+    Lx = 50
+
+    a_init = np.random.normal(size=L)
+    random_forces = np.zeros((L, Lx))
+    pinning_field = linear_interpolated_pinning_field(random_forces)
+
+    line = ElasticLine(L, L / 4, pinning_field=pinning_field)
+
+    a = a_init.copy()
+    w = -1
+    grad = line.gradient(a, w)
+
+    while (grad < 0).any():
+        w -= 1
+        grad = line.gradient(a, w)
+
+    sol = brute_rosso_krauth_other_spacing(a, w, line, gtol=1e-10, dir=-1)
+    assert sol.success
+
+    np.testing.assert_allclose(sol.x, w)
+    #example_strong_pinning()
+    #example_large_disp_force()
--- test/test_rosso_krauth.py
+++ test/test_rosso_krauth.py
@@ -2,13 +2,16 @@ import numpy as np
 
 from CrackFront.GenericElasticLine import ElasticLine
 from CrackFront.Optimization.fixed_radius_trustregion_newton_cg import trustregion_newton_cg
-from CrackFront.Optimization.RossoKrauth import linear_interpolated_pinning_field, brute_rosso_krauth
+from CrackFront.Optimization.RossoKrauth import (
+    linear_interpolated_pinning_field, brute_rosso_krauth,
+    brute_rosso_krauth_other_spacing
+    )
 import pytest
 
-
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth, brute_rosso_krauth_other_spacing])
 @pytest.mark.parametrize("Lx", [32, 33])
 @pytest.mark.parametrize("L", [32, 33])
-def test_no_pinning_field(L, Lx):
+def test_no_pinning_field(L, Lx, rosso_krauth):
     a_init = np.random.normal(size=L)
     random_forces = np.zeros((L, Lx))
     pinning_field = linear_interpolated_pinning_field(random_forces)
@@ -23,15 +26,15 @@ def test_no_pinning_field(L, Lx):
         w += 1
         grad = line.gradient(a, w)
 
-    sol = brute_rosso_krauth(a, w, line, gtol=1e-10)
+    sol = rosso_krauth(a, w, line, gtol=1e-10)
     assert sol.success
 
     np.testing.assert_allclose(sol.x, w)
 
-
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth, brute_rosso_krauth_other_spacing])
 @pytest.mark.parametrize("Lx", [32, 33])
 @pytest.mark.parametrize("L", [32, 33])
-def test_no_pinning_field_moving_left(L, Lx):
+def test_no_pinning_field_moving_left(rosso_krauth, L, Lx):
     a_init = np.random.normal(size=L)
     random_forces = np.zeros((L, Lx))
     pinning_field = linear_interpolated_pinning_field(random_forces)
@@ -46,13 +49,13 @@ def test_no_pinning_field_moving_left(L, Lx):
         w -= 1
         grad = line.gradient(a, w)
 
-    sol = brute_rosso_krauth(a, w, line, gtol=1e-10, dir=-1)
+    sol = rosso_krauth(a, w, line, gtol=1e-10, dir=-1)
     assert sol.success
 
     np.testing.assert_allclose(sol.x, w)
 
-
-def test_force_displacement_curve(plot=False):
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth, brute_rosso_krauth_other_spacing])
+def test_force_displacement_curve(rosso_krauth, plot=False):
     np.random.seed(0)
 
     L = 256
@@ -82,7 +85,7 @@ def test_force_displacement_curve(plot=False):
     a = np.zeros(L)
     for a_forcing in a_forcings:
         # print(a_forcing)
-        sol = brute_rosso_krauth(a, a_forcing, line, maxit=100000, gtol=gtol, )
+        sol = rosso_krauth(a, a_forcing, line, maxit=100000, gtol=gtol, )
         assert sol.success
         a = sol.x
         mean_a_RK.append(np.mean(a))
@@ -134,8 +137,8 @@ def test_force_displacement_curve(plot=False):
 
     np.testing.assert_allclose(mean_a_trust, mean_a_RK)
 
-
-def test_force_displacement_curve_hysteresis(plot=False):
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth, brute_rosso_krauth_other_spacing])
+def test_force_displacement_curve_hysteresis(rosso_krauth, plot=False):
     np.random.seed(0)
 
     L = 256
@@ -169,7 +172,7 @@ def test_force_displacement_curve_hysteresis(plot=False):
         # print(a_forcing)
         dir = 1 if a_forcing > a_forcing_prev else -1
         # print(dir)
-        sol = brute_rosso_krauth(a, a_forcing, line, maxit=100000, gtol=gtol, dir=dir)
+        sol = rosso_krauth(a, a_forcing, line, maxit=100000, gtol=gtol, dir=dir)
         assert sol.success
         a = sol.x
         mean_a_RK.append(np.mean(a))
@@ -209,3 +212,219 @@ def test_force_displacement_curve_hysteresis(plot=False):
     # %%
 
     np.testing.assert_allclose(mean_a_trust, mean_a_RK)
+
+
+
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth, brute_rosso_krauth_other_spacing])
+@pytest.mark.parametrize("Lx", [32, 33])
+@pytest.mark.parametrize("L", [32, 33])
+def test_no_pinning_field(L, Lx, rosso_krauth):
+    a_init = np.random.normal(size=L)
+    random_forces = np.zeros((L, Lx))
+    pinning_field = linear_interpolated_pinning_field(random_forces)
+
+    line = ElasticLine(L, L / 4, pinning_field=pinning_field)
+
+    a = a_init.copy()
+    w = 1
+    grad = line.gradient(a, w)
+
+    while (grad > 0).any():
+        w += 1
+        grad = line.gradient(a, w)
+
+    sol = rosso_krauth(a, w, line, gtol=1e-10)
+    assert sol.success
+
+    np.testing.assert_allclose(sol.x, w)
+
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth, brute_rosso_krauth_other_spacing])
+@pytest.mark.parametrize("Lx", [32, 33])
+@pytest.mark.parametrize("L", [32, 33])
+def test_no_pinning_field_moving_left(rosso_krauth, L, Lx):
+    a_init = np.random.normal(size=L)
+    random_forces = np.zeros((L, Lx))
+    pinning_field = linear_interpolated_pinning_field(random_forces)
+
+    line = ElasticLine(L, L / 4, pinning_field=pinning_field)
+
+    a = a_init.copy()
+    w = -1
+    grad = line.gradient(a, w)
+
+    while (grad < 0).any():
+        w -= 1
+        grad = line.gradient(a, w)
+
+    sol = rosso_krauth(a, w, line, gtol=1e-10, dir=-1)
+    assert sol.success
+
+    np.testing.assert_allclose(sol.x, w)
+
+@pytest.mark.parametrize("rosso_krauth", [brute_rosso_krauth,
+                                        pytest.param(brute_rosso_krauth_other_spacing, marks=pytest.mark.skip(reason="does not support the periodic BC"))
+                                          ])
+def test_force_displacement_curve(rosso_krauth, plot=False):
+    np.random.seed(0)
+
+    L = 256
+    Lx = 256
+
+    random_forces = np.random.normal(size=(L, Lx)) * 1.  # *0.05
+    pinning_field = linear_interpolated_pinning_field(random_forces)
+
+    line = ElasticLine(L, L / 16, pinning_field=pinning_field)
+
+    a_init = np.zeros(L) + 1e-14
+    a = a_init.copy()
+    w = 1
+
+    grad = line.gradient(a, w)
+
+    while (grad > 0).any():
+        w += 1
+        grad = line.gradient(a, w)
+
+    gtol = 1e-6
+
+    a_forcings = w + np.linspace(0, 1, 200)
+
+    # %%
+    mean_a_RK = []
+    a = np.zeros(L)
+    for a_forcing in a_forcings:
+        # print(a_forcing)
+        sol = rosso_krauth(a, a_forcing, line, maxit=100000, gtol=gtol, )
+        assert sol.success
+        a = sol.x
+        mean_a_RK.append(np.mean(a))
+
+    # %%
+    mean_a_trust_coarse = []
+    a = np.zeros(L)
+    for a_forcing in a_forcings:
+        wrapped_gradient = lambda a: line.gradient(a, a_forcing)
+        sol = trustregion_newton_cg(
+            x0=a, gradient=wrapped_gradient,
+            hessian_product=lambda a, p: line.hessian_product(p, a),
+            trust_radius=1 / 2,
+            maxiter=1000000,
+            gtol=gtol, )
+        a = sol.x
+        assert sol.success
+        mean_a_trust_coarse.append(np.mean(a))
+
+    # %%
+    mean_a_trust = []
+    a = np.zeros(L)
+    for a_forcing in a_forcings:
+        wrapped_gradient = lambda a: line.gradient(a, a_forcing)
+        sol = trustregion_newton_cg(
+            x0=a, gradient=wrapped_gradient,
+            hessian_product=lambda a, p: line.hessian_product(p, a),
+            trust_radius=1 / 8,
+            gtol=gtol,
+            maxiter=10000000)
+        a = sol.x
+        assert sol.success
+
+        mean_a_trust.append(np.mean(a))
+
+    # %%
+    if plot:
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots()
+
+        ax.plot(a_forcings, a_forcings - mean_a_RK, label="KR")
+        ax.plot(a_forcings, a_forcings - mean_a_trust, "+", label="TR, safe")
+
+        ax.plot(a_forcings, a_forcings - mean_a_trust_coarse, "x", label="TR")
+        ax.legend()
+        plt.show(block=True)
+    # %%
+
+    np.testing.assert_allclose(mean_a_trust, mean_a_RK)
+
+@pytest.mark.parametrize("rosso_krauth", [#brute_rosso_krauth,
+    brute_rosso_krauth_other_spacing])
+def test_force_displacement_curve_hysteresis_do_not_wrap_periodic(rosso_krauth, plot=False):
+    """
+
+    Here the starting position is well inside the random field, so that the solver doesn't need to support
+    the periodic boundary conditions
+    """
+    np.random.seed(0)
+
+    L = 128
+    Lx = 128
+
+    random_forces = np.random.normal(size=(L, Lx)) * 1.  # *0.05
+    pinning_field = linear_interpolated_pinning_field(random_forces)
+
+    line = ElasticLine(L, L / 16, pinning_field=pinning_field)
+
+    gtol = 1e-10
+    w = 20
+    # Do an initial configuration
+    sol = trustregion_newton_cg(
+        x0=np.zeros(L), gradient=lambda a: line.gradient(a, w),
+        hessian_product=lambda a, p: line.hessian_product(p, a),
+        trust_radius=1 / 8,
+        gtol=gtol,
+        maxiter=10000000)
+    assert sol.success, sol.message
+    a_init = sol.x
+
+    a_forcings = w + np.linspace(0, 20, 300)[1:]
+    a_forcings = np.concatenate([a_forcings, a_forcings[:-1][::-1]])
+
+    # %%
+    mean_a_RK = []
+    a = a_init.copy()
+    a_forcing_prev = 0
+    for a_forcing in a_forcings:
+        # print(a_forcing)
+        dir = 1 if a_forcing > a_forcing_prev else -1
+        # print(dir)
+        sol = rosso_krauth(a, a_forcing, line, maxit=10000000, gtol=gtol, dir=dir)
+        assert sol.success
+        a = sol.x
+        mean_a_RK.append(np.mean(a))
+        a_forcing_prev = a_forcing
+
+    print("RK done")
+
+    # %%
+    mean_a_trust = []
+    a = np.zeros(L)
+    for a_forcing in a_forcings:
+        # print(a_forcing)
+        wrapped_gradient = lambda a: line.gradient(a, a_forcing)
+        sol = trustregion_newton_cg(
+            x0=a, gradient=wrapped_gradient,
+            hessian_product=lambda a, p: line.hessian_product(p, a),
+            trust_radius=1 / 64,
+            gtol=gtol,
+            maxiter=100000000)
+        a = sol.x
+        assert sol.success
+
+        mean_a_trust.append(np.mean(a))
+
+    print("trust  done")
+    # %%
+    if True:
+        import matplotlib.pyplot as plt
+
+        fig, ax = plt.subplots()
+
+        ax.plot(a_forcings, a_forcings - mean_a_RK, label="KR")
+        ax.plot(a_forcings, a_forcings - mean_a_trust, "+", label="TR, safe")
+
+        ax.legend()
+        plt.show(block=True)
+    # %%
+
+    np.testing.assert_allclose(mean_a_trust, mean_a_RK)
+
