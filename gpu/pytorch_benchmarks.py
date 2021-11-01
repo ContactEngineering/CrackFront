9633d319711e97799cced690fd000ae31ae7eb7b
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-11-01 07:16:04 +0100

    DOC: a[mask] += 1 is significantly slower than a+= mask

--- gpu/pytorch_benchmarks.py
+++ gpu/pytorch_benchmarks.py
@@ -123,5 +123,149 @@ ax.legend()
 ax.set_xscale("log")
 ax.set_yscale("log")
 # -
+3276832768
 
+
+# +
+times_numpy = []
+times_cuda3 = []
+
+times_cuda2 = []
+times_cuda = []
+repeats=10
+Ls = [256, 1024, 4096, 16384, 65536, 262144]
+for L in Ls:
+    values_numpy = np.random.normal(size=L)
+    values_numpy2 = np.random.normal(size=L)
+    
+    values_cuda = torch.from_numpy(values_numpy).cuda()
+    values_cuda2 = torch.from_numpy(values_numpy).cuda()
+    
+    values2_cuda = torch.from_numpy(values_numpy).cuda()
+    values2_cuda2 = torch.from_numpy(values_numpy).cuda()
+    
+    values3_cuda = torch.from_numpy(values_numpy).cuda()
+    values3_cuda2 = torch.from_numpy(values_numpy).cuda()
+    
+    start_time = time.time()
+    for i in range(repeats):
+        mask = values_numpy > values_numpy2
+        values_numpy[mask] = values_numpy2[mask]
+    times_numpy.append(time.time() - start_time)
+    
+    
+    
+    start_time = time.time()
+    for i in range(repeats):
+        mask = values_cuda > values_cuda2
+        values_cuda[mask] += 1
+    times_cuda.append(time.time() - start_time)
+    
+    start_time = time.time()
+    for i in range(repeats):
+        values3_cuda += values_cuda > values_cuda2
+    times_cuda3.append(time.time() - start_time)
+    
+    #start_time = time.time()
+    #for i in range(repeats):
+    #    values2_cuda = torch.maximum(values2_cuda, values2_cuda2)
+    #times_cuda2.append(time.time() - start_time)
+# -
+
+
+v = torch.tensor([0, 1, 2, ])
+
+v
+
+v += v < 1
+
+v
+
+# +
+fig, ax = plt.subplots()
+
+ax.plot(Ls, times_numpy, "o", label="numpy, mask")
+ax.plot(Ls, times_cuda, "x", label="cuda, mask")
+#ax.plot(Ls, times_cuda2, "+", label="cuda, max")
+ax.plot(Ls, times_cuda3, "<", label="cuda, += mask")
+
+
+ax.legend()
+
+ax.set_xscale("log")
+ax.set_yscale("log")
+# -
+colloc_point_above
+
+
+
+# +
+times_numpy = []
+times_cuda3 = []
+
+times_cuda2 = []
+times_cuda = []
+repeats=10
+Ls = [256, 1024, 4096, 16384, 65536, 262144]
+for L in Ls:
+    values_numpy = np.random.normal(size=L)
+    values_numpy2 = np.random.normal(size=L)
+    
+    values_cuda = torch.from_numpy(values_numpy).cuda()
+    values_cuda2 = torch.from_numpy(values_numpy).cuda()
+    
+    values2_cuda = torch.from_numpy(values_numpy).cuda()
+    values2_cuda2 = torch.from_numpy(values_numpy).cuda()
+    
+    values3_cuda = torch.from_numpy(values_numpy).cuda()
+    values3_cuda2 = torch.from_numpy(values_numpy).cuda()
+    
+    start_time = time.time()
+    for i in range(repeats):
+        mask = values_numpy > values_numpy2
+        values_numpy[mask] = values_numpy2[mask]
+    times_numpy.append(time.time() - start_time)
+    
+    
+    
+    start_time = time.time()
+    for i in range(repeats):
+        mask = values_cuda > values_cuda2
+        values_cuda[mask] += 1
+    times_cuda.append(time.time() - start_time)
+    
+    start_time = time.time()
+    for i in range(repeats):
+        values3_cuda += values_cuda > values_cuda2
+    times_cuda3.append(time.time() - start_time)
+    
+    start_time = time.time()
+    for i in range(repeats):
+        values2_cuda.add_(values_cuda > values_cuda2)
+    times_cuda2.append(time.time() - start_time)
+# -
+
+
+v = torch.tensor([0, 1, 2, ])
+
+v
+
+v += v < 1
+
+v
+
+# +
+fig, ax = plt.subplots()
+
+ax.plot(Ls, times_numpy, "o", label="numpy, mask")
+ax.plot(Ls, times_cuda, "x", label="cuda, mask")
+ax.plot(Ls, times_cuda2, "+", label="cuda, add_")
+ax.plot(Ls, times_cuda3, "<", label="cuda, += mask")
+
+
+ax.legend()
+
+ax.set_xscale("log")
+ax.set_yscale("log")
+# -
 
