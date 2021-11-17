f7e3c401e35c2c816a2f316beee42d79c8f774fb
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2021-11-17 08:54:40 +0100

    TST: check that the derivative is alright in the linear interpolator

--- test/test_propagate_sphere_trust_region.py
+++ test/test_propagate_sphere_trust_region.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from Adhesion.ReferenceSolutions import JKR
 from NuMPI.IO.NetCDF import NCStructuredGrid
 
@@ -44,6 +45,51 @@ def test_propagate_sphere_trust_region_vs_JKR():
     nc = NCStructuredGrid("trust.nc")
     np.testing.assert_allclose(nc.force, [JKR.force(penetration=p) for p in nc.penetration[:]], rtol=1e-06)
     np.testing.assert_allclose(nc.mean_radius, [JKR.contact_radius(penetration=p) for p in nc.penetration[:]], rtol=1e-06)
+
+@pytest.mark.skip("just plotting")
+def test_linear_interpolated_pinning_field_derivative():
+    npx_front = 16
+    n_radii = 100
+
+    sample_radii = np.linspace(0.5, 6, n_radii)
+    w_values = np.ones((npx_front, 1)) * sample_radii.reshape(1, -1)
+    w_values = np.random.normal(size=(npx_front, n_radii))
+
+    piecewise_linear_w = linear_interpolated_pinning_field_equaly_spaced(w_values * sample_radii * 2 * np.pi / npx_front,
+                                                                        sample_radii)
+    cf = SphereCFPenetrationEnergyConstGcPiecewiseLinearField(piecewise_linear_w)
+
+    a = 3 + np.random.uniform(0, 1, size=npx_front)
+    da = np.random.uniform(0, 1, size=npx_front)
+
+    w = cf.piecewise_linear_w(a)
+
+    if True:
+        hs = np.array([1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5,
+                       1e-6, 1e-7])
+        rms_errors = []
+        for h in hs:
+            dw = cf.piecewise_linear_w(a + h * da) - w
+            dw_from_derivative = cf.piecewise_linear_w(a, der="1") * h * da
+            rms_errors.append(np.sqrt(np.mean((dw_from_derivative - dw) ** 2)))
+
+        # Visualize the quadratic convergence of the taylor expansion
+        # What to expect:
+        # Taylor expansion: g(x + h ∆x) - g(x) = Hessian * h * ∆x + O(h^2)
+        # We should see quadratic convergence as long as h^2 > g epsmach,
+        # the precision with which we are able to determine ∆g.
+        # What is the precision with which the hessian product is made ?
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+        ax.plot(hs, rms_errors# / hs ** 2
+                , "+-")
+        print(rms_errors)
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.grid(True)
+        plt.show()
+
+
 
 def test_linear_interpolated_pinning_field_vs_JKR():
     """
